751cbc1df8b3362990c7e9e3bf72c40c98487896
Aman-Pal287 <palaman9786@gmail.com>
2025-10-02 01:25:13 +0530

    feat(order): add cancelOrderById endpoint to allow users to cancel pending orders

--- order/src/routes/order.routes.js
+++ order/src/routes/order.routes.js
@@ -16,9 +16,18 @@ router.post(
 /* GET /api/orders/me */
 router.get("/me", createAuthMiddleware(["user"]), orderController.getMyOrders);
 
+/* POST /api/orders/:id/cancel */
+router.post(
+  "/:id/cancel",
+  createAuthMiddleware(["user"]),
+  orderController.cancelOrderById
+);
 
-/* GET /api/orders/:id */ 
-router.get("/:id" , createAuthMiddleware(["user" , "admin"]) , orderController.getOrderById)
-
+/* GET /api/orders/:id */
+router.get(
+  "/:id",
+  createAuthMiddleware(["user", "admin"]),
+  orderController.getOrderById
+);
 
 module.exports = router;
