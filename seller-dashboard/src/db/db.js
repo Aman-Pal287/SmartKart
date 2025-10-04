92511c21848240c528e7803d62889033ed281103
Aman-Pal287 <palaman9786@gmail.com>
2025-10-04 23:18:18 +0530

    feat: data base connetion done

--- seller-dashboard/src/db/db.js
+++ seller-dashboard/src/db/db.js
@@ -4,9 +4,10 @@ const logger = require("../utils/logger");
 async function connectToDB() {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    logger.info("Connted to database");
+    logger.info("Connected to database");
   } catch (error) {
     logger.error("Database connection error: ", error);
+    process.exit(1);
   }
 }
 
