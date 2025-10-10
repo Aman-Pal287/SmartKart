9cd2da7de632e3a0d70f6ef8980c29cfcca66a5a
Aman-Pal287 <palaman9786@gmail.com>
2025-10-10 15:13:31 +0530

    feat: add root endpoint to return service status message

--- ai-buddy/src/app.js
+++ ai-buddy/src/app.js
@@ -2,4 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Ai-Buddy Service is up and running",
+  });
+});
+
 module.exports = app;
