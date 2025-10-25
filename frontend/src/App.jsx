5da6892cbd69fb2a1c02c82b2d33b6e0dfeb2480
Aman-Pal287 <palaman9786@gmail.com>
2025-10-25 14:00:20 +0530

    feat(auth): add Docker configuration and .dockerignore for authentication service feat(frontend): implement click handler in App component and add basic styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,31 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
+import { useEffect, useRef, useState } from "react";
 
 const App = () => {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </>
-  );
+  // useEffect(() => {
+  //   const handleKeyDown = (e) => {
+  //     if (e.key === "ArrowRight") {
+  //       console.log("Right arrow key pressed!");
+  //     }
+  //   };
+
+  //   window.addEventListener("keydown", handleKeyDown);
+
+  //   return () => {
+  //     window.removeEventListener("keydown", handleKeyDown);
+  //   };
+  // }, []);
+  let ref = useRef(0);
+
+  function handleClick() {
+    console.log( ref.current);
+    
+    ref.current = ref.current + 1;
+    alert("You clicked " + ref.current + " times!");
+  }
+
+  return <button  onClick={handleClick}>Click me!</button>;
 };
 
 export default App;
